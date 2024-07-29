47b89b3f8204dfd8129045641bcc9369847d8375
karthikoff <karthikrajgokul@gmail.com>
2024-07-30 00:15:15 +0530

    fitness-rewards

--- src/components/Rewards.jsx
+++ src/components/Rewards.jsx
@@ -3,7 +3,7 @@ import { ethers } from "ethers";
 import { fitnessRewardsABI } from "../FitnessRewardsABI"; // Import ABI
 import Navbar from "./NavBar"; 
 
-const contractAddress = "0x28f5FE7d6857e49Ff563be5BC82Bb79ACe1F1B7d"; // Replace with your contract address
+const contractAddress = "0x9B2E397A389DE50f02528821032DC8A4aE057884"; // Replace with your contract address
 
 const isValidEthereumAddress = (address) => {
   return (
